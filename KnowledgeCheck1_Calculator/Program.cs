27e8980aa2f96a716917de02fcefe3c2988690db
nathanroyhawes <nathanhawes@proton.me>
2023-02-13 21:48:41 -0500

    first commit

--- KnowledgeCheck1_Calculator/Program.cs
+++ KnowledgeCheck1_Calculator/Program.cs
@@ -51,9 +51,20 @@ namespace KnowledgeCheck1_Calculator
                     break;
 
                 case "3":
-                    // Add code here
-                    break;
+                    Console.WriteLine("Enter 2 integers to multiply");
+                    var multiplyNumber1 = Console.ReadLine();
+                    var multiplyNumber2 = Console.ReadLine();
 
+                    if (int.TryParse(multiplyNumber1, out int multNumOne) && int.TryParse(multiplyNumber2, out int multNumTwo))
+                    {
+                        Console.Write($"{multiplyNumber1} * {multiplyNumber2} = ");
+                        Console.Write(calculator.Multiply(multNumOne, multNumTwo));
+                    }
+                    else
+                    {
+                        Console.WriteLine("One or more numbers is not an int");
+                    }
+                    break;
                 case "4":
                     Console.WriteLine("Enter 2 integers to divide");
                     var divideNumber1 = Console.ReadLine();
